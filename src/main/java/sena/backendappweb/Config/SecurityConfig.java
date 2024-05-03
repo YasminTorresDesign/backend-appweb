343d14a19c450e4f6a327e1a086ead66c927fde5
YasminTorresDeveloper <126827320+YasminTorresDeveloper@users.noreply.github.com>
2024-05-03 18:40:06 -0500

    se creo el controlador de usuario y otros archivos

--- src/main/java/sena/backendappweb/Config/SecurityConfig.java
+++ src/main/java/sena/backendappweb/Config/SecurityConfig.java
@@ -2,6 +2,7 @@ package sena.backendappweb.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,6 +28,8 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(authRequest ->
             authRequest
+                .requestMatchers(HttpMethod.GET).permitAll()
+                .requestMatchers(HttpMethod.OPTIONS).permitAll()
                 .requestMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
         )
---
+++ src/main/java/sena/backendappweb/User/UserRequest.java
@@ -0,0 +1,18 @@
+package sena.backendappweb.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+    int id;
+    String username;
+    String firstname;
+    String lastname;
+    String country;
+}
